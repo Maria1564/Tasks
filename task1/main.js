bb5e5026c7568d6b33090a2e5c729540abfa05fe
Maria <salyginamaria156@gmail.com>
2023-11-14 19:35:07 +0300

    start

---
+++ task1/main.js
@@ -0,0 +1,21 @@
+// const post = {
+//     title: 'foo',
+//     body: 'bar',
+//     userId: 1
+//   };
+  
+//   fetch('https://jsonplaceholder.typicode.com/posts', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(post)
+//   })
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
+
+// function sum(a, b) {
+//     return a + b;
+//   }
+//   module.exports = sum;
